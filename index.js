6267314d0982a910f64eb6ed9af9d86c4288cda0
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2023-01-13 19:08:35 +0300

    implement isPalindrome fn and add the pseudocode of it

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  let flag = true
+  for (let i= 0; i < word.length ; i++){
+    if (flag){
+      if (word[i] !== word[(word.length -1 ) - i]){
+        
+        flag = false   
+      }
+      else{
+         flag = true
+      }
+    }
+  }
+     return flag
+  }
+
 
 /* 
   Add your pseudocode here
+  iterate over the word from the left 
+      inside the loop
+      another iteration from the right
+      compare the elements from left to elements from right
+      make a flag to indicate the equality if true or not
+
+    return flag
 */
 
 /*
@@ -20,6 +40,10 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("");
+
+  
 }
 
 module.exports = isPalindrome;
+
